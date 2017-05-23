4d16d183db5e8910f704e712aae2fe1ae3249282
Christopher Schmautz <cyanrave@gmail.com>
2017-05-22 21:39:40 -0500

    fix: moved the application into a different module, out of init.

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,2 @@
 
-from flask import Flask
-
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
-
-from src import views
+from src import app
